89e6a3e0934f9c90d74ce1e070945e33c41d6841
UUsome <495050243@qq.com>
2019-07-26 17:16:04 +0800

    sf

--- 22.系统基模学习/011.成长上限.py
+++ 22.系统基模学习/011.成长上限.py
@@ -30,46 +30,49 @@ from matplotlib import pyplot as plt
     到某一临界 越努力反作用越大 (S型，或者 前期指数，后期震荡)
 '''
  
-#  logistic(初始数量，初始自然增长率，成长极限，时间):
+#  logistic(初始数量，初始自然增长率，成长极限，时间，标志): f=1显示速度，2显示累计
 def logistic(n,r,k,t,f):
     for i in range(1,t):
-      if n<k/2:
-        n=n+n*r
-      else:  
-        n = n + n*r*(1-n/k)
-        r = r*(1-n/k)
+      n = n + n*r*(1-n/k)
+      r = r*(1-n/k)
     if f==1:
         return r
     else:
         return n
 
-# GrowthState =20
-# stimulus =0.03
-x = np.arange(1,25,1) 
+# 定义时间区间
+x = np.arange(1,100,1) 
+
+#初始化参数
+n = 1
+r = 1
+k = 100
+
 
 #subplots(nrows=1, ncols=1, sharex=False, sharey=False, squeeze=True,subplot_kw=None, gridspec_kw=None, **fig_kw)
 fig, axs = plt.subplots(2, 1)   #两行，一列 。两个图像
 
-y1 = [logistic(100,0.0001,500,i,1) for i in x]
-y2 = [logistic(100,0.0025,500,i,1) for i in x]
-y3 = [logistic(100,0.0030,500,i,1) for i in x]
-y4 = [logistic(100,0.0400,500,i,1) for i in x]
-y5 = [logistic(100,0.0500,500,i,1) for i in x]
-y6 = [logistic(100,0.0600,500,i,1) for i in x]
-y7 = [logistic(100,0.0700,500,i,1) for i in x]
-y8 = [logistic(100,0.0800,500,i,1) for i in x]
-# y9 = [logistic(100,0.9000,500,i,1) for i in x]
 
+''' #多个对比
+y1 = [logistic(n,r*0.1,k,i,1) for i in x]
+y2 = [logistic(n,r*0.1,k,i,1) for i in x]
+y3 = [logistic(n,r*0.1,k,i,1) for i in x]
+y4 = [logistic(n,r*0.1,k,i,1) for i in x]
+y5 = [logistic(n,r*0.1,k,i,1) for i in x]
+y6 = [logistic(n,r*0.1,k,i,1) for i in x]
+y7 = [logistic(n,r*0.1,k,i,1) for i in x]
+y8 = [logistic(n,r*0.1,k,i,1) for i in x]
+y9 = [logistic(n,r*0.1,k,i,1) for i in x]
 
-z1 = [logistic(100,0.0001,500,i,2) for i in x]
-z2 = [logistic(100,0.0025,500,i,2) for i in x]
-z3 = [logistic(100,0.0030,500,i,2) for i in x]
-z4 = [logistic(100,0.0400,500,i,2) for i in x]
-z5 = [logistic(100,0.0500,500,i,2) for i in x]
-z6 = [logistic(100,0.0600,500,i,2) for i in x]
-z7 = [logistic(100,0.0700,500,i,2) for i in x]
-z8 = [logistic(100,0.0800,500,i,2) for i in x]
-# z9 = [logistic(100,0.9000,500,i,2) for i in x]
+z1 = [logistic(n,r*0.1,k,i,2) for i in x]
+z2 = [logistic(n,r*0.1,k,i,2) for i in x]
+z3 = [logistic(n,r*0.1,k,i,2) for i in x]
+z4 = [logistic(n,r*0.1,k,i,2) for i in x]
+z5 = [logistic(n,r*0.1,k,i,2) for i in x]
+z6 = [logistic(n,r*0.1,k,i,2) for i in x]
+z7 = [logistic(n,r*0.1,k,i,2) for i in x]
+z8 = [logistic(n,r*0.1,k,i,2) for i in x]
+z9 = [logistic(n,r*0.1,k,i,2) for i in x]
 
 axs[0].plot(x,y1,label='y1') 
 axs[0].plot(x,y2,label='y2') 
@@ -79,10 +82,9 @@ axs[0].plot(x,y5,label='y5')
 axs[0].plot(x,y6,label='y6') 
 axs[0].plot(x,y7,label='y7') 
 axs[0].plot(x,y8,label='y8') 
-# axs[0].plot(x,y9,label='y9') 
+axs[0].plot(x,y9,label='y9') 
 
 axs[0].legend(loc='upper right', shadow=False)
-
 
 axs[1].plot(x,z1,label='z1') 
 axs[1].plot(x,z2,label='z2') 
@@ -92,15 +94,23 @@ axs[1].plot(x,z5,label='z5')
 axs[1].plot(x,z6,label='z6') 
 axs[1].plot(x,z7,label='z7') 
 axs[1].plot(x,z8,label='z8') 
-# axs[1].plot(x,z9,label='z9') 
+axs[1].plot(x,z9,label='z9') 
 
+axs[1].legend(loc='upper right', shadow=False)
+'''
+
+
+y = [logistic(n,r*0.1,k,i,1) for i in x]
+z = [logistic(n,r*0.1,k,i,2) for i in x]
+axs[0].plot(x,y,label='y') 
+axs[1].plot(x,z,label='显示累计') 
 axs[1].legend(loc='upper right', shadow=False)
 
 
 plt.title("成长上线") 
 plt.xlabel("时间") 
 plt.ylabel("成长速度") 
-# print(x,y7)
+# print(x,y,z)
 plt.show()
 
 # y = [logistic(10,0.1,50,i,1) for i in x]
@@ -110,3 +120,13 @@ plt.show()
 # print(y)
 # print(z)
 
+
+
+
+
+'''  函数分析：
+
+
+
+
+'''
